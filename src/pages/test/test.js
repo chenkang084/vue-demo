8918b5213b5ff4548f8067a48d9c4f77cbf74370
chenkang1 <chenkang1@lenovo.com>
2017-04-06 12:56:05 +0800

    add test page

---
+++ src/pages/test/test.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import './style.scss'
+
+export default Vue.component('test', {
+    template: require('./template.html')
+    // template: '<div>A custom component!</div>'
+})
